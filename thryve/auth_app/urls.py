7aad4c944b8a86dde1b1e959239240b933b7f228
siv-a-siv <verinkai727@gmail.com>
2025-10-06 18:12:59 +0800

    setup(registration): added url path for both base url and app url for navigation

---
+++ thryve/auth_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from auth_app import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
+]
