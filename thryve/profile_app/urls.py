8ff8a8f3f48b8f2b671bc7d0efa4e36033468efb
Justine Vilocura <justine.vilocura@cit.edu>
2025-10-12 17:58:53 +0800

    feature(profile): added profile customization and with uis

--- thryve/profile_app/urls.py
+++ thryve/profile_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('profile/business/', views.business_profile_view, name='business_profile'),
     path('home/', views.business_profile_view, name='profile_home'),  # new alias
+    path('customization/', views.profile_customization_view, name='profile_customization'),
 ]
