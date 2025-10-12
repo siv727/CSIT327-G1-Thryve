96c3ed4243f0ce5c0570194da60c140d36c55dcb
siv-a-siv <verinkai727@gmail.com>
2025-10-13 03:41:04 +0800

    fix(auth): refactored field validation to contain vars for reuse

--- thryve/auth_app/forms.py
+++ thryve/auth_app/forms.py
@@ -5,6 +5,7 @@ from django import forms
 from .models import CustomUser
 
 class RegistrationForm(UserCreationForm):
+    INVALID_NAME_PATTERN = r'[\'\"@_!#$%^&*()<>?/|}{~:]'
     first_name = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'First Name',
         'class': 'w-full px-3 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 transition-colors',
@@ -41,21 +42,21 @@ class RegistrationForm(UserCreationForm):
 
     def clean_first_name(self):
         first_name = self.cleaned_data['first_name']
-        pattern = r'[\'\"@_!#$%^&*()<>?/|}{~:]'
-        if first_name.strip() == "":
+        stripped_name = first_name.strip()
+        if stripped_name == "":
             raise forms.ValidationError("Please enter a first name.")
-        elif re.search(r'\d', first_name.strip()) or re.search(pattern, first_name.strip()):
+        elif re.search(r'\d', stripped_name) or re.search(self.INVALID_NAME_PATTERN, stripped_name):
             raise forms.ValidationError("Please enter a valid first name.")
         else:
             return first_name
 
     def clean_last_name(self):
         last_name = self.cleaned_data['last_name']
-        pattern = r'[\'\"@_!#$%^&*()<>?/|}{~:]'
-        if last_name.strip() == "":
-            raise forms.ValidationError("Please enter a first name.")
-        elif re.search(r'\d', last_name.strip()) or re.search(pattern, last_name.strip()):
-            raise forms.ValidationError("Please enter a valid first name.")
+        stripped_name = last_name.strip()
+        if stripped_name == "":
+            raise forms.ValidationError("Please enter a last name.")
+        elif re.search(r'\d', stripped_name) or re.search(self.INVALID_NAME_PATTERN, stripped_name):
+            raise forms.ValidationError("Please enter a valid last name.")
         else:
             return last_name
 
@@ -97,7 +98,7 @@ class LoginForm(AuthenticationForm):
         'placeholder': 'Email Address',
         'class': 'w-full px-3 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 transition-colors',
     }))
-    password = forms.CharField(widget=forms.EmailInput(attrs={
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Password',
         'class': 'w-full px-3 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 transition-colors',
     }))
