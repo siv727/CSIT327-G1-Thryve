e38b6b405328774a4fe77fc1a0c0fcdd7b9f197a
siv-a-siv <verinkai727@gmail.com>
2025-10-19 03:40:34 +0800

    setup(thryve): added db conn checking to avoid stale conn - issue: max clients reached f

--- thryve/thryve/settings.py
+++ thryve/thryve/settings.py
@@ -91,6 +91,8 @@ DATABASES = {
     )
 }
 
+DATABASES['default']['ATOMIC_REQUESTS'] = False
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
