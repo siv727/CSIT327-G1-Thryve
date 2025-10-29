5c0ccadf308efe24da2d093c2e501cb500deae18
siv-a-siv <verinkai727@gmail.com>
2025-10-29 17:25:50 +0800

    tech(thryve): added reqs text and env variables

--- thryve/thryve/settings.py
+++ thryve/thryve/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ax(1a2@8xxkile-79$s437!c4l38*%-z01qw_ejrq*f%^ju5v-'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
