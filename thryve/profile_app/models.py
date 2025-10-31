f40a3533c1a56ab4f4350ccc6870efd40773f963
Shnyzer <143820204+Shnyzer@users.noreply.github.com>
2025-10-31 22:57:46 +0800

    feature(profile): added changes

--- thryve/profile_app/models.py
+++ thryve/profile_app/models.py
@@ -7,7 +7,7 @@ class BusinessProfile(models.Model):
     company_name = models.CharField(max_length=255, blank=True)
     industry = models.CharField(max_length=100, blank=True)
     description = models.TextField(blank=True, null=True)
-    street_address = models.CharField(max_length=255, blank=True)
+    street_address = models.CharField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=100, blank=True)
     state = models.CharField(max_length=100, blank=True)
     zip_code = models.CharField(max_length=20, blank=True)
