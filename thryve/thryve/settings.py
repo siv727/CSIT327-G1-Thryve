a1ba65d7f6c62ee4596aa81ff94e79c346d632e8
siv-a-siv <verinkai727@gmail.com>
2025-12-01 18:36:02 +0800

    feat(marketplace): init marketplace app with basic struct and URL routing

--- thryve/thryve/settings.py
+++ thryve/thryve/settings.py
@@ -65,7 +65,11 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates'),
-                 os.path.join(BASE_DIR, 'auth_app', 'templates')],
+                 os.path.join(BASE_DIR, 'auth_app', 'templates'),
+                 os.path.join(BASE_DIR, 'booking_app', 'templates'),
+                 os.path.join(BASE_DIR, 'connection_app', 'templates'),
+                 os.path.join(BASE_DIR, 'marketplace_app', 'templates'),
+                 os.path.join(BASE_DIR, 'profile_app', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
