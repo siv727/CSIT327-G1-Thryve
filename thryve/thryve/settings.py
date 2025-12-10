7a474960bba95b5e4bb84dd4c890bc138a67641c
siv-a-siv <verinkai727@gmail.com>
2025-12-10 08:29:23 +0800

    fix(listings): fix ui scroll

--- thryve/thryve/settings.py
+++ thryve/thryve/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'booking_app', 'static'),
-    os.path.join(BASE_DIR, 'marketplace_app', 'static'),
+    os.path.join(BASE_DIR, 'marketplace_app', 'static', 'marketplace_app'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
